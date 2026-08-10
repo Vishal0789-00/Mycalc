71b75571dc8f84912578cd226c2755e0613da340
Vishal0789-00 <vk8807266@gmail.com>
2026-08-10 18:56:59 +0530

    Update service worker cache version and file handling

--- service-worker.js
+++ service-worker.js
@@ -1,58 +1,52 @@
-const CACHE_NAME = "mycalc-v4";
+const CACHE_NAME = "mycalc-v5";
 
-const FILES_TO_CACHE = [
-  "index.html",
-  "manifest.json",
-  "service-worker.js",
-  "icon.png"
+const FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon.png"
 ];
 
 self.addEventListener("install", event => {
-
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES);
+    })
   );
 
   self.skipWaiting();
-
 });
 
-
 self.addEventListener("activate", event => {
-
   event.waitUntil(
-
     caches.keys().then(keys => {
-
       return Promise.all(
-
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
       );
-
     })
-
   );
 
   self.clients.claim();
-
 });
 
-
 self.addEventListener("fetch", event => {
-
   event.respondWith(
-
-    caches.match(event.request)
+    fetch(event.request)
       .then(response => {
+        const copy = response.clone();
 
-        return response || fetch(event.request);
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, copy);
+        });
 
+        return response;
       })
-
+      .catch(() => {
+        return caches.match(event.request);
+      })
   );
-
 });
